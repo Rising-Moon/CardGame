35825e08f5ae272dabd8295c64eb3a54ffcf7c79
孟昱成 <767644624@qq.com>
2019-08-05 15:23:48 +0800

    修改AB包地址

--- Assets/Editor/CreateAssetBundles.cs
+++ Assets/Editor/CreateAssetBundles.cs
@@ -8,7 +8,7 @@ public class CreateAssetBundles
 {
     [MenuItem("Assets/Build AssetBundle")]
     static void BuildBundles(){
-        string AssetBundleDirectory = "Assets/AssetBundles";
+        string AssetBundleDirectory = Application.streamingAssetsPath + "/AssetBundles";
         if (!Directory.Exists(AssetBundleDirectory))
             Directory.CreateDirectory(AssetBundleDirectory);
         BuildPipeline.BuildAssetBundles(AssetBundleDirectory, BuildAssetBundleOptions.None,
